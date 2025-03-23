c14bdeda05b581252c2f8efecc29723c7c835372
Roman E. Gerasimov <r.e.gerasimov@inp.nsk.su>
2025-03-23 18:56:56 +0700

    [fix]: installation on Windows

--- Packages/makeRNL.m
+++ Packages/makeRNL.m
@@ -7,17 +7,17 @@ PACKAGE="RNL";
 
 thisfile=If[$Notebooks,NotebookFileName[],$InputFileName];
 SetDirectory[DirectoryName[thisfile]];
-appdir=$UserBaseDirectory <> "/Applications/"<>PACKAGE<>"/";
+appdir=FileNameJoin[{$UserBaseDirectory, "Applications", PACKAGE}];
 files = Join[FileNames["*.wl"],FileNames["*.m"]];
 If[files==={},Print["No "<>"*.m, *.wl files in "<>Directory[]<>". Changed nothing, quitting..."];Quit[]];
 files=DeleteCases[DeleteDuplicatesBy[files,FileBaseName],FileNameTake[thisfile]];
 Quiet[CreateDirectory[appdir]];
 Scan[Function[file,
-fn=appdir<>FileBaseName[file]<>".m";
+fn=FileNameJoin[{appdir, FileBaseName[file]<>".m"}];
 If[FileExistsQ[fn],write=Replace[InputString["You already have "<>fn<>". Overwrite? (y/n)"],{"y"|"yes"->True,_->False}],write=True];
 If[write,
 init=OpenWrite[fn];
-WriteString[init,"Get[\""<>Directory[]<>"/"<>file<>"\"]"];
+WriteString[init, ToString@StringForm["Get[``]", ToString[FileNameJoin[{Directory[], file}], InputForm]]];
 Close[init];
 Print["Installed shortcut for "<>file<>". Use\n  <<"<>PACKAGE<>"`"<>FileBaseName[file]<>"`"]]],
 files]
